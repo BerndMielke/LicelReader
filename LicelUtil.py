ae467af182f6ce4621f5ad992db02b6bbda36e80
Bernd Mielke <mielke@licel.com>
2023-11-17 13:12:32 +0100

    smooth function added

--- LicelUtil.py
+++ LicelUtil.py
@@ -1,20 +1,22 @@
 import numpy as np
 import os
 
+
+
 def offset_correction(physData, start, stop):
       arr = physData[start:stop] 
-      return physData - np.mean(arr) 
+      return physData - np.mean(arr)
 
-def smooth(physData, filterWidth):
-      """ return a smoothed array based on the physData input 
+def smoothed_signal(data,filterWidth): 
+      """ return a smoothed array based on the data input 
         Parameters
         ----------
-        physData : numpy array
+        data : numpy array
         filterWidth : int
             The width of the filtering the larger the number the stronger the
             filtering
         """
       kernel_size = filterWidth
       kernel = np.ones(kernel_size) / kernel_size
-      return np.convolve(physData, kernel, mode='same')
+      return np.convolve(data, kernel, mode='same')
 
