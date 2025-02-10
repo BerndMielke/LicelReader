e7f69548644798ad54a875eb9edae071acc38a9e
Bernd Mielke <mielke@licel.com>
2025-02-10 13:36:26 +0100

    call it downsampling

--- LicelUtil.py
+++ LicelUtil.py
@@ -44,4 +44,22 @@ def smoothed_signal(data: np.ndarray, filterWidth: int) -> np.ndarray :
       kernel_size = filterWidth
       kernel = np.ones(kernel_size) / kernel_size
       return np.convolve(data, kernel, mode='same')
+def downsampling(data: np.ndarray, exponent: int) -> np.ndarray :
+      """
+      accumulating the array in steps of 0  no accumulation, 1 add two bins
+      2 add 4 bins...
+      Parameters
+      ----------
+      data : np.ndarray
+            data input array 
+      exponent: int
+            how many accumulation steps are needed
+      Returns
+      -------
+      np.ndarray :
+            downsampled array
+      """
+      oversampling_Factor = 1 << exponent
+      y = np.split(oversampling_Factor)
+      return np.sum(y, axis = 1)
 
